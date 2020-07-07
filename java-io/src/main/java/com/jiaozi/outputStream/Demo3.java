a7baa629329c09a7ea191d567f49f7e869fb7845
xiam <651307866@qq.com>
2020-07-07 21:30:31 +0800

    输出流

--- java-io/src/main/java/com/jiaozi/outputStream/Demo3.java
+++ java-io/src/main/java/com/jiaozi/outputStream/Demo3.java
@@ -6,14 +6,16 @@ import java.io.IOException;
 /**
  * @Description:
  * @Author: xiam
- * @Create: 2020-07-06
+ * @Create: 2020-07-07
  **/
-
-public class Demo1 {
+public class Demo3 {
     public static void main(String[] args) throws IOException {
         FileInputStream fileInputStream = new FileInputStream("demo1.txt");
-        int read = fileInputStream.read();
-        System.out.println((char)read);
+        byte[] b = new byte[2];
+        int len = 0;
+        while ((len= fileInputStream.read(b))!=-1){
+            System.out.println(new String(b));
+        }
         fileInputStream.close();
     }
 }
--- java-io/src/main/java/com/jiaozi/outputStream/demo2.java
+++ java-io/src/main/java/com/jiaozi/outputStream/demo2.java
@@ -6,14 +6,15 @@ import java.io.IOException;
 /**
  * @Description:
  * @Author: xiam
- * @Create: 2020-07-06
+ * @Create: 2020-07-07
  **/
-
-public class Demo1 {
+public class demo2 {
     public static void main(String[] args) throws IOException {
         FileInputStream fileInputStream = new FileInputStream("demo1.txt");
-        int read = fileInputStream.read();
-        System.out.println((char)read);
+        int b = 0;
+        while ((b=fileInputStream.read())!=-1){
+            System.out.println((char)b);
+        }
         fileInputStream.close();
     }
 }
